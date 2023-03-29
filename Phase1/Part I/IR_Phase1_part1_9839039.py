ae5e40eed6a9b31d309f1608a35fcd196be4a265
Precioux <uni.mahdipour@gmail.com>
2023-03-29 23:53:11 +0330

    Part1 - doc

--- Phase1/Part I/IR_Phase1_part1_9839039.py
+++ Phase1/Part I/IR_Phase1_part1_9839039.py
@@ -32,10 +32,14 @@ def to_normalize(input_text):
 
 def to_tokenize(input_text):
     print('Tokenizing...')
+    # remove all non-alphanumeric characters from the input_text
+    input_text = re.sub(r'[^\w\s]','',input_text)
+    # tokenize the cleaned text
     output_text = word_tokenize(input_text)
     print(f'input : {input_text} ')
     print(f'output : {output_text}')
     return output_text
+
 
 
 def to_remove_stop_words(input_text):
@@ -63,7 +67,9 @@ def preprocess(data):
 
     for docID, doc in data.items():
         preprocessed_doc = {}
+        print(f'Doc Content: {doc["content"]}')
         pure_content = re.sub(f'[{punctuation}؟،٪×÷»«]+', '', doc['content'])
+        print(f'Pure Content : {pure_content}')
         preprocessed_doc['content'] = to_stem(to_remove_stop_words(to_tokenize(to_normalize(pure_content))))
         preprocessed_doc['url'] = doc['url']
         preprocessed_data[docID] = preprocessed_doc
@@ -74,16 +80,21 @@ def preprocess(data):
 # # Check the changes in preprocessed data
 # data_test = {
 #     'docID_1': {
-#         'title': 'Title of Document 1',
-#         'content': 'این یک متن نمونه است که برای تست پردازش متن به کار می‌رود.',
-#         'url': 'http://example.com/document1'
+#         'title': 'Test Dox',
+#         'content': 'این یک متن نمونه‌ی فارسی است که حاوی بسیاری از کلمات رایج مثل به، از، و، با، در و ... می‌باشد و برای بررسی کارکرد توابع پیش‌پردازش به کار می‌رود.',
+#         'url': 'http://example.com/test_document'
 #     }
 # }
 #
 # preprocessed_data = preprocess(data_test)
-#
 # # Check the changes in preprocessed data
 # print(preprocessed_data['docID_1']['content'])
 
+# real data preprocessing
 preprocessed_data = preprocess(data)
-print(preprocessed_data)
+# print(preprocessed_data)
+
+# save preprocessed data as a JSON file
+output_file_path = 'C:/Users/Samin/Desktop/University/Term 7/Information Retrieval/Project/Data/IR_data_news_12k_preprocessed.json'
+with open(output_file_path, 'w', encoding='utf-8') as f:
+    json.dump(preprocessed_data, f, ensure_ascii=False, indent=4)
